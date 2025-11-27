abcbfca733dcf245b43eeb15df6524fbb497f151
candyP <2689825705@qq.com>
2025-11-27 16:45:15 +0800

    init proj

---
+++ src/main/java/com/example/luofushan/LuofushanApplication.java
@@ -0,0 +1,13 @@
+package com.example.luofushan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuofushanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuofushanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/luofushan/LuofushanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.luofushan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuofushanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
