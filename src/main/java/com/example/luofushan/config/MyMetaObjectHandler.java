1725899e0ff8af8d03e94a08ed0822e41413bbc6
candyP <2689825705@qq.com>
2025-11-28 18:01:39 +0800

    feat:修改时间格式为LocalDateTime

--- src/main/java/com/example/luofushan/config/MyMetaObjectHandler.java
+++ src/main/java/com/example/luofushan/config/MyMetaObjectHandler.java
@@ -4,20 +4,24 @@ import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import org.apache.ibatis.reflection.MetaObject;
 import org.springframework.stereotype.Component;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 @Component
 public class MyMetaObjectHandler implements MetaObjectHandler {
 
     @Override
-    public void insertFill(MetaObject metaObject){
-        strictInsertFill(metaObject, "createdAt", Date::new, Date.class);
-        strictInsertFill(metaObject, "updatedAt", Date::new, Date.class);
+    public void insertFill(MetaObject metaObject) {
+        // LocalDateTime 类型
+        strictInsertFill(metaObject, "createdAt", LocalDateTime::now, LocalDateTime.class);
+        strictInsertFill(metaObject, "updatedAt", LocalDateTime::now, LocalDateTime.class);
         strictInsertFill(metaObject, "delflag", () -> 0, Integer.class);
     }
 
+    /**
+     * 更新时自动填充
+     */
     @Override
-    public void updateFill(MetaObject metaObject){
-        strictUpdateFill(metaObject, "updatedAt", Date::new, Date.class);
+    public void updateFill(MetaObject metaObject) {
+        strictUpdateFill(metaObject, "updatedAt", LocalDateTime::now, LocalDateTime.class);
     }
 }
--- src/main/java/com/example/luofushan/dao/entity/BaseDO.java
+++ src/main/java/com/example/luofushan/dao/entity/BaseDO.java
@@ -4,7 +4,7 @@ import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableField;
 import lombok.Data;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 @Data
 public class BaseDO {
@@ -14,14 +14,14 @@ public class BaseDO {
      * 在执行 INSERT 操作时，自动给该字段赋值
      */
     @TableField(fill = FieldFill.INSERT)
-    private Date createdAt;
+    private LocalDateTime createdAt;
 
     /**
      * 修改时间
      * 在执行 INSERT UPDATE 操作时，自动给该字段赋值
      */
     @TableField(fill = FieldFill.INSERT_UPDATE)
-    private Date updatedAt;
+    private  LocalDateTime updatedAt;
 
     /**
      * 删除标识 0：未删除 1：已删除
