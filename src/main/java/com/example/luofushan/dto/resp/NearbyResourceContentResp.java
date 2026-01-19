bf97a1f37dc2a55c27682f7578f076716cf3c604
candyP <2689825705@qq.com>
2026-01-19 19:36:38 +0800

    feat: 返回附近信息添加经纬度

--- src/main/java/com/example/luofushan/dto/resp/NearbyResourceContentResp.java
+++ src/main/java/com/example/luofushan/dto/resp/NearbyResourceContentResp.java
@@ -9,6 +9,8 @@ public class NearbyResourceContentResp {
     private Long id;
     private String name;
     private Double distance;       // 单位米
+    private Double latitude;
+    private Double longitude;
     private Integer hotScore;
     private String coverImg;
     private LocalDateTime createdAt;
